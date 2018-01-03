724e0363ea5dc787c2bddc4ca95ee0823b530332
cdwilliamszenoss <31999703+cdwilliamszenoss@users.noreply.github.com>
2018-01-03 15:58:48 -0600

    Update events-transforms.py

--- events-transforms.py
+++ events-transforms.py
@@ -1,7 +1,7 @@
 
 if evt.severity == 5 and 'absolute value of offset' in evt.summary:
 #  evt._action = 'history'
-#  Use eventState to suppress the event to the console. To show suppressed events, select "Suppressed" on Status button
+#  Use eventState to suppress the event on the console. To show suppressed events, select "Suppressed" on Status button
    evt.eventState = 2
 
 # Use to Drop events
