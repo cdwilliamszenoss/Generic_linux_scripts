6a7fda6d89bc9cdc01c2d5c184cf9862792f30d9
cdwilliamszenoss <31999703+cdwilliamszenoss@users.noreply.github.com>
2018-04-20 12:27:06 -0500

    dockerContainerCapacityUsage

--- dockerContainerCapacityUsage.sh
+++ dockerContainerCapacityUsage.sh
@@ -1,8 +1,10 @@
 #Identify containers using 70% and above capacity
 
-
 for i in $(df | awk '!/shm/ { if (($5 ~ /[[7-9][0-9]/) || ($5 ~/100/)) {print $6  }}'| awk -v FS='/' '{print $7}'); do docker inspect $(docker ps -q) | grep -E "DeviceName|Hostname" | grep -A1 $i; done | grep Hostname 
 
-
-
 serviced service status | grep $Hostname
+
+
+#Get container hostname
+
+for i in $(docker ps -q); do docker inspect $i --format '{{.Config.Hostname}}' ; done | grep hostnameID
