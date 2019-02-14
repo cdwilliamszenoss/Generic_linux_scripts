cd7cb9c91c81e6704f7410acdcd7e11e31eb4c16
cdwilliamszenoss <31999703+cdwilliamszenoss@users.noreply.github.com>
2019-02-14 00:17:11 -0600

    Update reset_linuxSwap.sh

--- reset_linuxSwap.sh
+++ reset_linuxSwap.sh
@@ -1,3 +1,12 @@
 # Used to reset swap memory. Execute via cron. Set MAX_SWAP_VALUE to desired value.
 
-export MAX_SWAP_VALUE=4000; if [[ $(egrep -o "*.[0-9]*." <(swapon -s |awk 'NR>1 {print $4}')) -gt $MAX_SWAP_VALUE ]]; then echo "Turn off"; swapoff -a; sleep 5;fi; while true; do if [[ ! $(swapon -s | awk 'NR>1 {print $1}') ]]; then echo "Turn ON";swapon -a; sleep 5;else echo "UP"; break;fi;done
+#!/bin/bash
+
+export MAX_SWAP_VALUE=400; if [[ $(egrep -o "*.[0-9]*." <(/sbin/swapon -s |awk 'NR>1 {print $4}')) -gt $MAX_SWAP_VALUE ]]; then echo "Turning Swap OFF"; /sbin/swapoff -a; sleep 5;fi; while [[ ! $(/sbin/swapon -s | awk 'NR>1 {print $1}') ]]; do echo "Turning Swap ON";/sbin/swapon -a;done
+
+
+# List processes using swap
+ for i in /proc/*/status; do  awk -v var=$i '/Name/{NAME=$0;next} /VmSwap/ && $2 > 0 {print NAME"\t"$0"\t"var} ' $i  2>/dev/null; done | sort -t ":" -k3 -n |column -t
+
+
+--------------------------
