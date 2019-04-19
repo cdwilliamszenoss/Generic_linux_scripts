3b472ab471ecb9a6616aa034d49819c3ab5c8982
cdwilliamszenoss <31999703+cdwilliamszenoss@users.noreply.github.com>
2019-04-19 17:48:08 -0500

    Update reset_linuxSwap.sh

--- reset_linuxSwap.sh
+++ reset_linuxSwap.sh
@@ -1,12 +1,9 @@
 # Used to reset swap memory. Execute via cron. Set MAX_SWAP_VALUE to desired value.
 
-#!/bin/bash
-
 export MAX_SWAP_VALUE=400; if [[ $(egrep -o "*.[0-9]*." <(/sbin/swapon -s |awk 'NR>1 {print $4}')) -gt $MAX_SWAP_VALUE ]]; then echo "Turning Swap OFF"; /sbin/swapoff -a; sleep 5;fi; while [[ ! $(/sbin/swapon -s | awk 'NR>1 {print $1}') ]]; do echo "Turning Swap ON";/sbin/swapon -a;done
 
 
-# List processes using swap
- for i in /proc/*/status; do  awk -v var=$i '/Name/{NAME=$0;next} /VmSwap/ && $2 > 0 {print NAME"\t"$0"\t"var} ' $i  2>/dev/null; done | sort -t ":" -k3 -n |column -t
+# List all processes using Swap memory. Report generated in the tmp directory.
+for i in /proc/*/status; do  awk -v var=$i '/Name/{NAME=$0;next} /VmSwap/ && $2 > 0 {print NAME"\t"$0"\t"var} ' $i  2>/dev/null; done | sort -t ":" -k3 -n |column -t | tee -a /tmp/swap_usage-$(hostname).log
 
 
---------------------------
