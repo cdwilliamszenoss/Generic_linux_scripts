f31c96d60f35dbec7e1f226b3bfa4396330e21c9
cdwilliamszenoss <31999703+cdwilliamszenoss@users.noreply.github.com>
2019-08-30 18:17:30 -0500

    sethostid

--- sethostid.py
+++ sethostid.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 # To re-use the original hostid. Convert from hexadecimal to decimal
-# Convert Hex to Decimal : int(hex_value,16)
-# Convert Decimal to Hex : hex(decimal_value)
 
+# Convert Hex to Decimal 
+#
+# To convert a hex_value to a decimal. The int() accepts a string and base 16. 
+#  int('0x5a0a6f24',16) 
+#
+# Convert a Decimal to Hex : hex(1510633252) 
+#
 # Pass decimal value into pack method
 
 from struct import pack
