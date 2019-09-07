5d06c949c6bee828ad6b5115f8f40312796ff655
cdwilliamszenoss <31999703+cdwilliamszenoss@users.noreply.github.com>
2019-09-07 00:43:39 -0500

    Update reset_linux_swap.sh

--- reset_linux_swap.sh
+++ reset_linux_swap.sh
@@ -2,7 +2,7 @@
 # will automatically renable swap on the system.
 #
 # Used to reset swap memory. Execute via cron. Set MAX_SWAP_VALUE to desired value.
-# 
+ 
 
 export MAX_SWAP_VALUE=400; if [[ $(egrep -o "*.[0-9]*." <(/sbin/swapon -s |awk 'NR>1 {print $4}')) -gt $MAX_SWAP_VALUE ]]; then echo "Turning Swap OFF"; /sbin/swapoff -a; sleep 5;fi; while [[ ! $(/sbin/swapon -s | awk 'NR>1 {print $1}') ]]; do echo "Turning Swap ON";/sbin/swapon -a;done
 
